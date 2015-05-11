4a04b8d3b310513a219b8f67fab7785c1ec599f2
LEOBAINI <leobaini@hotmail.com>
2015-05-11 01:55:56 -0300

    bp

--- pruebaMA/src/test/java/pruebaMaven/pruebaMA/Tests.java
+++ pruebaMA/src/test/java/pruebaMaven/pruebaMA/Tests.java
@@ -1,14 +1,21 @@
 package pruebaMaven.pruebaMA;
 
+
+
 import org.junit.Test;
 
 import junit.framework.Assert;
-import junit.framework.TestCase;
 
-public class Tests extends TestCase {
+
+public class Tests {
 	@Test
 	public void test1(){
 		Assert.assertFalse(false);
+		
+	}
+	@Test
+	public void test2(){
+		Assert.assertTrue(true);
 		
 	}
 	
