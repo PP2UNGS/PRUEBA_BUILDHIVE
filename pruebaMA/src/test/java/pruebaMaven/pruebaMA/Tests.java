7fc603b423ef59c71f7c81760aea40070687146e
LEOBAINI <leobaini@hotmail.com>
2015-05-11 01:01:23 -0300

    n

---
+++ pruebaMA/src/test/java/pruebaMaven/pruebaMA/Tests.java
@@ -0,0 +1,18 @@
+package pruebaMaven.pruebaMA;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class Tests extends TestCase {
+	@Test
+	public void test1(){
+		Assert.assertFalse(false);
+		
+	}
+	
+	
+	
+
+}
